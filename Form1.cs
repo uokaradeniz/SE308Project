1c7ee2d0a823a843d12f5213ade47a95bb7c2d3e
Umutcan Elçi <umutcanelci@yaani.com>
2024-05-02 14:44:35 +0300

    Database Conenction and Commands

--- Form1.cs
+++ Form1.cs
@@ -8,10 +8,14 @@ namespace SE308Project
 
         public int UserCountA { get => userCountA; set => userCountA = value; }
         public int UserCountB { get => userCountB; set => userCountB = value; }
-
+        string a = "READ UNCOMMITTED";
+        string b = "READ COMMITTED";
+        string c = "REPEATABLE READ";
+        string d = "SERIALIZABLE";
         public Form1()
         {
             InitializeComponent();
+             string connectionString = "Data Source=UMUTCAN\\SQLEXPRESS;Initial Catalog=AdventureWorks2022;Integrated Security=True;Encrypt=False;";
         }
 
         private void btnStartSim_Click(object sender, EventArgs e)
@@ -21,7 +25,7 @@ namespace SE308Project
 
             Thread userA = new Thread(UserA);
             Thread userB = new Thread(UserB);
-
+            
             userA.Start();
             userB.Start();
 
@@ -31,6 +35,30 @@ namespace SE308Project
         private void UserA()
         {
             lblUserA.Text = "User A Users: " + UserCountA;
+            Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+            
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        for (int i = 0; i < 100; i++)
+                        {
+                            using (SqlCommand command = connection.CreateCommand())
+                            {
+                                command.CommandText = $"SET TRANSACTION ISOLATION LEVEL {a}; BEGIN TRANSACTION; " +
+                                                      "SELECT SUM(Sales.SalesOrderDetail.OrderQty) FROM Sales.SalesOrderDetail " +
+                                                      "WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE " +
+                                                      "Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID " +
+                                                      "AND Sales.SalesOrderHeader.OrderDate BETWEEN '20110101' AND '20151231' " +
+                                                      "AND Sales.SalesOrderHeader.OnlineOrderFlag = 1); COMMIT TRANSACTION;";
+            
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                    }
+            
+                    stopwatch.Stop();
+                    Console.WriteLine("Type A transactions completed in: " + stopwatch.Elapsed.TotalSeconds + " seconds" + "\n Isolation Level: " + a);
             //float beginTime = tic();
             //for (int i = 0; i < UserCountA; i++)
             //{
@@ -40,11 +68,30 @@ namespace SE308Project
         private void UserB()
         {
             lblUserB.Text = "User B Users: " + UserCountB;
-            //float beginTime = tic();
-            //for (int i = 0; i < UserCountB; i++)
-            //{
-
-            //}
+            Stopwatch stopwatch = new Stopwatch();
+                        stopwatch.Start();
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                                {
+                                    connection.Open();                        
+                                    for (int i = 0; i < 100; i++)
+                                    {
+                                        using (SqlCommand command = connection.CreateCommand())
+                                        {
+                                            
+                                            command.CommandText = $"SET TRANSACTION ISOLATION LEVEL {b}; BEGIN TRANSACTION; " +
+                                                                  "UPDATE Sales.SalesOrderDetail SET UnitPrice = UnitPrice * 10.0 / 10.0 " +
+                                                                  "WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE " +
+                                                                  "Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID " +
+                                                                  "AND Sales.SalesOrderHeader.OrderDate BETWEEN '20110101' AND '20151231' " +
+                                                                  "AND Sales.SalesOrderHeader.OnlineOrderFlag = 1); COMMIT TRANSACTION;";
+                        
+                                            command.ExecuteNonQuery();
+                                        }
+                                    }
+                                }
+                        
+                                stopwatch.Stop();
+                                Console.WriteLine("Type B transactions completed in: " + stopwatch.Elapsed.TotalSeconds + " seconds" + "\n Isolation Level: " + b);
         }
 
         private void button3_Click(object sender, EventArgs e)
