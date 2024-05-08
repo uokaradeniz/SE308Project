84bc6c3a495bdd16418f655b54f6c46b009e8e8b
Umutcan Elçi <umutcanelci@yaani.com>
2024-05-08 15:30:52 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,8 @@ namespace SE308Project
         public int UserCountA { get => userCountA; set => userCountA = value; }
         public int UserCountB { get => userCountB; set => userCountB = value; }
 
-        string connectionString = "Data Source=UGUROGUZHANPC\\SQLEXPRESS;Initial Catalog=AdventureWorks2022;Integrated Security=True;Encrypt=False;";
+        //string connectionString = "Data Source=UGUROGUZHANPC\\SQLEXPRESS;Initial Catalog=AdventureWorks2022;Integrated Security=True;Encrypt=False;";
+        string connectionString = "Data Source=UMUTCAN\\SQLEXPRESS;Initial Catalog=AdventureWorks2022;Integrated Security=True";
         Stopwatch stopwatch;
 
         public Form1()
@@ -39,27 +40,27 @@ namespace SE308Project
 
         private void UserA()
         {
-            Thread.Sleep(10);
+            
             txt_EventLog.AppendText("User A Users: " + UserCountA + "\n");
 
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    connection.Open();
-            //    for (int i = 0; i < userCountA; i++)
-            //    {
-            //        using (SqlCommand command = connection.CreateCommand())
-            //        {
-            //            command.CommandText = $"SET TRANSACTION ISOLATION LEVEL {cmb_Isolation.SelectedText}; BEGIN TRANSACTION; " +
-            //                                  "SELECT SUM(Sales.SalesOrderDetail.OrderQty) FROM Sales.SalesOrderDetail " +
-            //                                  "WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE " +
-            //                                  "Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID " +
-            //                                  "AND Sales.SalesOrderHeader.OrderDate BETWEEN '20110101' AND '20151231' " +
-            //                                  "AND Sales.SalesOrderHeader.OnlineOrderFlag = 1); COMMIT TRANSACTION;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open(); 
+                for (int i = 0; i < 2; i++)
+               {
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED; BEGIN TRANSACTION; " +
+                                              "SELECT SUM(Sales.SalesOrderDetail.OrderQty) FROM Sales.SalesOrderDetail " +
+                                              "WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE " +
+                                              "Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID " +
+                                              "AND Sales.SalesOrderHeader.OrderDate BETWEEN '20110101' AND '20151231' " +
+                                              "AND Sales.SalesOrderHeader.OnlineOrderFlag = 1); COMMIT TRANSACTION;";
 
-            //            command.ExecuteNonQuery();
-            //        }
-            //    }
-            //}
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
 
             txt_EventLog.AppendText("Type A transactions completed in: " + string.Format("{0:mm\\:ss}", stopwatch.Elapsed) + " seconds" + "\n");
 
@@ -69,25 +70,25 @@ namespace SE308Project
         {
             txt_EventLog.AppendText("User B Users: " + UserCountB + "\n");
 
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    connection.Open();
-            //    for (int i = 0; i < userCountB; i++)
-            //    {
-            //        using (SqlCommand command = connection.CreateCommand())
-            //        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                for (int i = 0; i < 2; i++)
+                {
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
 
-            //            command.CommandText = $"SET TRANSACTION ISOLATION LEVEL {cmb_Isolation.SelectedText}; BEGIN TRANSACTION; " +
-            //                                  "UPDATE Sales.SalesOrderDetail SET UnitPrice = UnitPrice * 10.0 / 10.0 " +
-            //                                  "WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE " +
-            //                                  "Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID " +
-            //                                  "AND Sales.SalesOrderHeader.OrderDate BETWEEN '20110101' AND '20151231' " +
-            //                                  "AND Sales.SalesOrderHeader.OnlineOrderFlag = 1); COMMIT TRANSACTION;";
+                        command.CommandText = "SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED; BEGIN TRANSACTION; " +
+                                              "UPDATE Sales.SalesOrderDetail SET UnitPrice = UnitPrice * 10.0 / 10.0 " +
+                                              "WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE " +
+                                              "Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID " +
+                                              "AND Sales.SalesOrderHeader.OrderDate BETWEEN '20110101' AND '20151231' " +
+                                              "AND Sales.SalesOrderHeader.OnlineOrderFlag = 1); COMMIT TRANSACTION;";
 
-            //            command.ExecuteNonQuery();
-            //        }
-            //    }
-            //}
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
 
             txt_EventLog.AppendText("Type B transactions completed in: " + string.Format("{0:mm\\:ss}", stopwatch.Elapsed) + " seconds\n");
         }
